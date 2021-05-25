7d876cd411854aea659b11e9eb0e048dfc0e2262
Albert Cavazos <arcavazos8371@gmail.com>
2021-05-25 12:27:53 -0500

    Grid added

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 let myImage = document.querySelector('img');
 
+
+
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/Carl-Jung.jpg') {
-      myImage.setAttribute('src','images/Diagram.jpg');  
+      myImage.setAttribute('src','images/Diagram.jpg');
+      
     }
     if(mySrc === 'images/Diagram.jpg') {
       myImage.setAttribute('src','images/Quote.jpg');  
@@ -12,5 +15,7 @@ myImage.onclick = function() {
       myImage.setAttribute('src','images/Carl-Jung.jpg');  
     }
 }
+
+
 
 
